f9d657c01ff47de80879c88731492937c12d1d74
ttr404 <trtt06.hu@gmail.com>
2025-09-22 15:31:55 -0600

    Add search insert position

--- Search Insert Position.py
+++ Search Insert Position.py
@@ -1,10 +1,26 @@
 class Solution(object):
+    # def searchInsert(self, nums, target):
+    #     for i in range(len(nums)):
+    #         if nums[i]==target:
+    #             return i
+    #         elif nums[i]>target:
+    #             return i
+    #         elif i == len(nums)-1:
+    #             return len(nums)
+    
+    # Binary Search
     def searchInsert(self, nums, target):
-        for i in range(len(nums)):
-            if nums[i]==target:
-                return i
-            elif nums[i]>target:
-                return i
-            elif i == len(nums)-1:
-                return len(nums)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         
+        return left
+    
+main = Solution()
+print(main.searchInsert([1,3,5,6], 5))
