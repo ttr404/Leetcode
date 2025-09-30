87220f3fe3e896ad1fc3950619d8f76a0719e4d7
ttr404 <trtt06.hu@gmail.com>
2025-09-29 23:14:05 -0600

    update

--- word pattern.py
+++ word pattern.py
@@ -3,16 +3,16 @@ class Solution(object):
         words = s.split()
         if len(pattern) != len(words):
             return False 
-        p2w = {}
-        w2p = {}
+        pattern_to_word_map = {}
+        word_to_pattern_map = {}
 
         for ch, w in zip(pattern, words):
-            if ch in p2w and p2w[ch] != w:
+            if ch in pattern_to_word_map and pattern_to_word_map[ch] != w:
                 return False
-            if w in w2p and w2p[w] != ch:
+            if w in word_to_pattern_map and word_to_pattern_map[w] != ch:
                 return False
-            p2w[ch] = w
-            w2p[w] = ch
+            pattern_to_word_map[ch] = w
+            word_to_pattern_map[w] = ch
 
         return True
         
